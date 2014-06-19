7529fcbb93af4d356b0f193030e080f7d22459c9
PhelanBavaria <antextides@gmail.com>
2014-06-19 17:49:54 +0200

    #12 partially done, also removed events in World

--- common/world.py
+++ common/world.py
@@ -4,6 +4,7 @@ import random
 from time import time
 import pygame
 import config
+import events
 from util.id_generator import id_generator
 from maps import maps
 
@@ -23,7 +24,7 @@ class World:
         'levels',
         'scents',
         'players',
-        'events',
+        'speed_mod',
         'clickables']
 
     def __init__(self, setup):
@@ -51,37 +52,34 @@ class World:
             ]
         }
         self.players = {}
-        self.events = {}
         self.clickables = pygame.sprite.Group()
         self.create = maps[setup['map_name']].create
         self.players = setup['players']
+        self.speed_mod = 1
         self.create(self)
         for name, player in self.players.items():
             self.players[name] = player(self)
+        events.keymap[pygame.K_SPACE] = self.toggle_speed
         print('setting up world done')
 
     def turn(self):
         turn_time = time()
         spt = turn_time - self.last_tick
-        if spt >= 1/config.TPS:
+        if spt*self.speed_mod >= 1/config.TPS:
             self.last_tick = time()
             self.entities.update()
             self.resources.update()
-            if self.current_tick in self.events.keys():
-                for event in self.events[self.current_tick]:
-                    event()
             if self.current_tick % config.TICKS_PER_DAY == 0:
                 self.day += 1
             self.current_tick += 1
-
-    def add_event(self, event, turn):
-        if turn not in self.events.keys():
-            self.events[turn] = []
-        self.events[turn].append(event)
-        # print(turn)
-        # print(len(self.events.items()))
 
     def randloc(self):
         x = random.randint(0, self.setup['map_size'][0])
         y = random.randint(0, self.setup['map_size'][1])
         return (x, y)
+
+    def toggle_speed(self):
+        if self.speed_mod == 0:
+            self.speed_mod = 1
+        elif self.speed_mod == 1:
+            self.speed_mod = 0
