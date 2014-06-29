8d1834a2a2aad000d20e2414fd8bef0b6912395c
PhelanBavaria <antextides@gmail.com>
2014-06-29 20:44:17 +0200

    #75 done, simplified code a lot

--- common/behaviors/return_home.py
+++ common/behaviors/return_home.py
@@ -5,12 +5,10 @@ from common import tiles
 
 
 def direct(ant):
-    objects = spritecollide(ant, ant.world.tiles, False)
-    for entrance in ant.colony.entrances:
-        if entrance in objects:
-            ant.colony.enter(ant)
-            return
-    ant.move(ant.exit_hole.rect.center)
-    if ant.resource and not ant.world.current_tick % 4:
-        tiles['scent'](ant)
-        #AntScent(ant, 'resource')
+    entrances = spritecollide(ant, ant.colony.entrances, False)
+    if entrances:
+        ant.colony.enter(ant)
+    else:
+        ant.move(ant.exit_hole.rect.center)
+        if ant.resource and not ant.world.current_tick % 4:
+            tiles['scent'](ant)
