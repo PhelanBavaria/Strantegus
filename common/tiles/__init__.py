495262d5baa0498fdbadd0449d6cb6b3fb619ead
PhelanBavaria <antextides@gmail.com>
2014-06-29 20:31:27 +0200

    #71 little bug fix

--- common/tiles/__init__.py
+++ common/tiles/__init__.py
@@ -3,11 +3,11 @@ from common.tiles.scent import Scent
 from common.tiles.resource import Resource
 from common.tiles.grass import Grass
 from common.tiles.dirt import Dirt
-from common.tiles.stone import Stone
-from common.tiles.water import Water
-from common.tiles.leave import Leave
-from common.tiles.sugar import Sugar
-from common.tiles.fungus import Fungus
+from common.tiles.resources.stone import Stone
+from common.tiles.resources.water import Water
+from common.tiles.resources.leave import Leave
+from common.tiles.resources.sugar import Sugar
+from common.tiles.resources.fungus import Fungus
 from common.tiles.dirt_wall import DirtWall
 from common.tiles.colony_entrance import ColonyEntrance
 from common.tiles.colony_wall import ColonyWall
